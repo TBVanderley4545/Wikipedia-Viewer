d0202d86d88f389967b29db23fdc95afe1f5b4b9
TBVanderley4545 <tbvanderley@gmail.com>
2017-11-21 01:36:47 -0500

    Multiple project updates.

--- js/app.js
+++ js/app.js
@@ -8,17 +8,14 @@ $(document).ready(function() {
   $form.click(function() {
     $('.magnifying-handle').animate({
       // top: '130%',
+      left: '105%',
+      top: '102%',
       width: '0px'
     }, transitionSpeed, function() {
       $form.animate({
         width: '400px'
       }, transitionSpeed);
     });
-
-    // $('.magnifying-handle').css({
-    //   top: '130%',
-    //   width: '0px'
-    // })
   });
 
   $('#quick-check').click(function() {
@@ -26,6 +23,8 @@ $(document).ready(function() {
       width: '30px'
     }, transitionSpeed, function() {
       $('.magnifying-handle').animate({
+        left: '90%',
+        top: '115%',
         width: '19px'
       });
     });
