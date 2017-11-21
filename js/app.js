7b661f997d1b497ae30b11d60e8c826eadfb2e15
TBVanderley4545 <tbvanderley@gmail.com>
2017-11-20 21:44:17 -0500

    Update to files.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
 $(document).ready(function() {
+  // Set reusable variables.
+  var transitionSpeed = 250;
+
   // Get reference to key UI components
   var $form = $('.search-form');
 
   $form.click(function() {
-    $(this).css({
-      width: '400px'
-    })
+    $('.magnifying-handle').animate({
+      // top: '130%',
+      width: '0px'
+    }, transitionSpeed, function() {
+      $form.animate({
+        width: '400px'
+      }, transitionSpeed);
+    });
+
+    // $('.magnifying-handle').css({
+    //   top: '130%',
+    //   width: '0px'
+    // })
   });
 
   $('#quick-check').click(function() {
-    $form.css({
+    $form.animate({
       width: '30px'
-    })
+    }, transitionSpeed, function() {
+      $('.magnifying-handle').animate({
+        width: '19px'
+      });
+    });
   });
 });
