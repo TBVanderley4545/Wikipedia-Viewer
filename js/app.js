6fe3ee6023a39097d3f4f5728da322c9ffad2b76
TBVanderley4545 <tbvanderley@gmail.com>
2017-12-10 03:44:18 -0500

    Format update.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Set reusable variables.
   var transitionSpeed = 250;
 
   // Get reference to key UI components
   var $form = $('.search-form');
 
-  $form.click(function() {
+  $form.click(function () {
     $('.magnifying-handle').animate({
       // top: '130%',
       left: '105%',
       top: '102%',
       width: '0px'
-    }, transitionSpeed, function() {
+    }, transitionSpeed, function () {
       $form.animate({
         width: '400px'
-      }, transitionSpeed, function() {
-        if(!$('.cancel-button').hasClass('cancel-visible')){
+      }, transitionSpeed, function () {
+        if (!$('.cancel-button').hasClass('cancel-visible')) {
           $('.cancel-button').addClass('cancel-visible');
         }
       });
     });
   });
 
-  $('#quick-check').click(function() {
-    if($('.cancel-button').hasClass('cancel-visible')) {
+  $('#quick-check').click(function () {
+    if ($('.cancel-button').hasClass('cancel-visible')) {
       $('.cancel-button').removeClass('cancel-visible');
     }
 
     window.setTimeout(function () {
       $form.animate({
         width: '30px'
-      }, transitionSpeed, function() {
+      }, transitionSpeed, function () {
         $('.magnifying-handle').animate({
           left: '90%',
           top: '115%',
