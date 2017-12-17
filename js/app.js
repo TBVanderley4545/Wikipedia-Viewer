3468cf70434c2b85ec737dc74fac5e5bce9664ac
TBVanderley4545 <tbvanderley@gmail.com>
2017-12-17 13:42:59 -0500

    Update to layout styles.

--- js/app.js
+++ js/app.js
@@ -10,21 +10,29 @@ $(document).ready(function() {
 
   $formInput.click(function() {
     if ($(this).hasClass('closed-input')) {
-      $('.magnifying-handle').animate({
-        left: '105%',
-        top: '102%',
-        width: '0px'
-      }, transitionSpeed, function() {
-        $formInput.removeClass('closed-input');
-        $form.animate({
-          width: '400px'
-        }, transitionSpeed, function() {
-          if (!$('.cancel-button').hasClass('cancel-visible')) {
-            $('.cancel-button').show();
-            $('.cancel-button').addClass('cancel-visible');
-          }
-        });
-      });
+      $('.magnifying-handle').animate(
+        {
+          left: '105%',
+          top: '102%',
+          width: '0px'
+        },
+        transitionSpeed,
+        function() {
+          $formInput.removeClass('closed-input');
+          $form.animate(
+            {
+              width: '400px'
+            },
+            transitionSpeed,
+            function() {
+              if (!$('.cancel-button').hasClass('cancel-visible')) {
+                $('.cancel-button').show();
+                $('.cancel-button').addClass('cancel-visible');
+              }
+            }
+          );
+        }
+      );
     }
   });
 
@@ -34,17 +42,25 @@ $(document).ready(function() {
     }
     window.setTimeout(function() {
       $formInput.addClass('closed-input');
-      $form.animate({
-        width: '30px'
-      }, transitionSpeed, function() {
-        $('.magnifying-handle').animate({
-          left: '90%',
-          top: '115%',
-          width: '19px'
-        }, transitionSpeed, function() {
-          $('.cancel-button').hide();
-        });
-      });
+      $form.animate(
+        {
+          width: '30px'
+        },
+        transitionSpeed,
+        function() {
+          $('.magnifying-handle').animate(
+            {
+              left: '90%',
+              top: '115%',
+              width: '19px'
+            },
+            transitionSpeed,
+            function() {
+              $('.cancel-button').hide();
+            }
+          );
+        }
+      );
     }, 250);
   });
 });
